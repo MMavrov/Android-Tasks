ac1514d75c717df3dac9729d10e6759fa04257ee
Milen Mavrov <m.mavr0v@yahoo.com>
2014-10-18 13:21:39 +0300

    Add .idea to gitignore list

--- ColorPrevewer/app/src/main/java/com/example/mavr0/colorprevewer/ColorPreviewer.java
+++ ColorPrevewer/app/src/main/java/com/example/mavr0/colorprevewer/ColorPreviewer.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
