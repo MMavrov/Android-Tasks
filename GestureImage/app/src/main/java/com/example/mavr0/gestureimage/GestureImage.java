32d607a46b1a8f85cac9f4a90cf978a06c46bdf3
Milen Mavrov <m.mavr0v@yahoo.com>
2014-10-29 23:30:27 +0200

    Working scaling and padding
    I now use a different method to change the picture: onTouchEvent of the
    parent activity. The problem/advantage is that the picture is changed,
    no matter if the touch is inside it or anywhere in the parent.

--- GestureImage/app/src/main/java/com/example/mavr0/gestureimage/GestureImage.java
+++ GestureImage/app/src/main/java/com/example/mavr0/gestureimage/GestureImage.java
@@ -2,74 +2,73 @@ package com.example.mavr0.gestureimage;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.view.MotionEventCompat;
-import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.ImageView;
 
 public class GestureImage extends Activity {
-    private int _xMovementDelta;
-    private int _yMovementDelta;
-    private double oldLength;
+    private ImageView gunman;
+    private int firtsTouchX;
+    private int firstTouchY;
+    private double firstTouchLength;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getActionMasked();
+
+        switch (action) {
+        case (MotionEvent.ACTION_DOWN):
+        case (MotionEvent.ACTION_POINTER_DOWN):
+            if (event.getPointerCount() == 1) {
+                firtsTouchX = (int) event.getX();
+                firstTouchY = (int) event.getY();
+
+            } else {
+                firstTouchLength = Math.sqrt(Math.pow(Math.abs(event.getX(1) - event.getX(0)), 2)
+                                     + Math.pow(Math.abs(event.getY(1) - event.getY(0)), 2));
+            }
+            break;
+
+        case (MotionEvent.ACTION_MOVE):
+            if (event.getPointerCount() == 1) {
+                MoveView(event.getX(), event.getY());
+            } else {
+                ScaleView(event.getX(0), event.getX(1), event.getY(0), event.getY(1));
+            }
+            break;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gesture_image);
 
-        final ImageView gunman = (ImageView) findViewById(R.id.gunman);
+        gunman = (ImageView) findViewById(R.id.gunman);
+    }
 
-        gunman.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
+    private void MoveView(float X, float Y){
+        // Moving
+        float dX = X - firtsTouchX;
+        float dY = Y - firstTouchY;
 
-                int action = event.getActionMasked();
-//                Log.w("event.toString()", event.toString());
-//                Log.w("action ID", String.valueOf(action));
+        firtsTouchX = (int) X;
+        firstTouchY = (int) Y;
 
-                switch (action) {
+        gunman.setTranslationX(gunman.getX() + dX);
+        gunman.setTranslationY(gunman.getY() + dY);
+    }
 
-                    case (MotionEvent.ACTION_DOWN):
-                    case (MotionEvent.ACTION_POINTER_DOWN):
-                        if (event.getPointerCount() == 1) {
-                            _xMovementDelta = (int) (event.getRawX() - v.getX());
-                            _yMovementDelta = (int) (event.getRawY() - v.getY());
+    private void ScaleView(float x0, float x1, float y0, float y1){
 
-                        } else {
-                            event.getActionIndex();
-                            oldLength =
-                                    Math.sqrt(Math.pow(Math.abs(event.getX(1) - event.getX(0)), 2) + Math.pow(Math.abs(event.getY(1) - event.getY(0)), 2));
-                        }
-                        break;
+        double newLength = Math.sqrt(Math.pow(Math.abs(x1 - x0), 2)
+                + Math.pow(Math.abs(y1 - y0), 2));
 
-                    case (MotionEvent.ACTION_MOVE):
-                        if (event.getPointerCount() == 1) {
-                            v.setTranslationX(event.getRawX() - _xMovementDelta);
-                            v.setTranslationY(event.getRawY() - _yMovementDelta);
+        double dL = (newLength / firstTouchLength) - 1;
 
-//                            Log.d("getX()", Float.toString(event.getX()));
-//                            Log.d("getY()", Float.toString(event.getY()));
-                        } else {
-                            double newLength = Math.sqrt(Math.pow(Math.abs(event.getX(1) - event.getX(0)), 2) + Math.pow(Math.abs(event.getY(1) - event.getY(0)), 2));
-                            double ratio = newLength / oldLength;
-                            v.setScaleX((float) (ratio));
-                            v.setScaleY((float) (ratio));
-                            v.invalidate();
+        firstTouchLength = newLength;
 
-//                            Log.w("Value of oldLength ", Double.toString(oldLength));
-//                            Log.w("Value of newLength ", Double.toString(newLength));
-                            Log.w("event.getX(0) ", Double.toString(event.getX(0)));
-                            Log.w("event.getY(0) ", Double.toString(event.getY(0)));
-                            Log.w("event.getX(1) ", Double.toString(event.getX(1)));
-                            Log.w("event.getY(1) ", Double.toString(event.getY(1)));
-//                            Log.w("rawX", Float.toString(event.getRawX()));
-//                            Log.w("rawY", Float.toString(event.getRawY()));
-                        }
-                        break;
-                    }
-                return true;
-            }
-        });
+        gunman.setScaleX((float) (gunman.getScaleX() + dL));
+        gunman.setScaleY((float) (gunman.getScaleY() + dL));
     }
 }
