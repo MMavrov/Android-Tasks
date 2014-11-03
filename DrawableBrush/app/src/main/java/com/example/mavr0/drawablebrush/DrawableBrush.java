3885cd41a911b2e1ae130b9efbe53a021285063e
Milen Mavrov <m.mavr0v@yahoo.com>
2014-11-04 00:06:25 +0200

    Drawing application
    The catch is that the brushes will be custom chosen images.

---
+++ DrawableBrush/app/src/main/java/com/example/mavr0/drawablebrush/DrawableBrush.java
@@ -0,0 +1,14 @@
+package com.example.mavr0.drawablebrush;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class DrawableBrush extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawable_brush);
+    }
+}
