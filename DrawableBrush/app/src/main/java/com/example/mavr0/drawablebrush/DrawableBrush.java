8535bb0baf0af18d5d56c90568b0ba7888dbce80
Milen Mavrov <m.mavr0v@yahoo.com>
2014-11-08 15:23:58 +0200

    Changeable brushes and gradient background
    TO DO: I had a problem with the radial gradient type.
    If gradientRadius >= 1 the whole rectangle paints in startColor value.
    If gradientRadius < 1 the whole rectangle paints in endColor value.

--- DrawableBrush/app/src/main/java/com/example/mavr0/drawablebrush/DrawableBrush.java
+++ DrawableBrush/app/src/main/java/com/example/mavr0/drawablebrush/DrawableBrush.java
@@ -8,12 +8,19 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 
 public class DrawableBrush extends Activity {
     LinearLayout root;
+
+    ImageButton guitarButton;
+    ImageButton golangButton;
+    ImageButton drumsButton;
+
     DrawableView drawableView;
+    Drawable currentBrush;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +30,43 @@ public class DrawableBrush extends Activity {
         root = (LinearLayout) findViewById(R.id.root);
         drawableView = new DrawableView(this);
         root.addView(drawableView);
+
+        guitarButton = (ImageButton) findViewById(R.id.guitar);
+        guitarButton.setOnClickListener(brushChangedListener);
+        guitarButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_green));
+
+        golangButton = (ImageButton) findViewById(R.id.golang);
+        golangButton.setOnClickListener(brushChangedListener);
+        golangButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_red));
+
+        drumsButton = (ImageButton) findViewById(R.id.drums);
+        drumsButton.setOnClickListener(brushChangedListener);
+        drumsButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_red));
+
+        currentBrush = getResources().getDrawable(R.drawable.guitar_icon);
     }
+
+    View.OnClickListener brushChangedListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (v == guitarButton){
+                currentBrush = getResources().getDrawable(R.drawable.guitar_icon);
+                guitarButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_green));
+                golangButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_red));
+                drumsButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_red));
+            }else if(v == golangButton) {
+                currentBrush = getResources().getDrawable(R.drawable.golang);
+                guitarButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_red));
+                golangButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_green));
+                drumsButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_red));
+            }else {
+                currentBrush = getResources().getDrawable(R.drawable.drums_icon);
+                guitarButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_red));
+                golangButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_red));
+                drumsButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_green));
+            }
+        }
+    };
 
     public class DrawableView extends View{
         private Bitmap  mBitmap;
@@ -43,13 +86,13 @@ public class DrawableBrush extends Activity {
 
         @Override
         public boolean onTouchEvent(MotionEvent event) {
-            Drawable currentBrush = DrawableBrush.this.getResources().getDrawable(R.drawable.guitar_icon);
             currentBrush.setBounds(
                     (int)event.getX(),
                     (int)event.getY(),
                     (int)event.getX()+100,
                     (int)event.getY()+100);
-            currentBrush.setAlpha(150);
+            // We call mutate() because otherwise we change the Drawable's property in the BrushesLayout
+            currentBrush.mutate().setAlpha(150);
             currentBrush.draw(mCanvas);
 
             invalidate();
